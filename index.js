f6ce63a1ba51781263a694154f7edb58ad32538f
jinnurain-jihan <zinnurainzihan171@gmail.com>
2022-10-20 18:36:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function myFunction1(){
+    // var p=document.querySelector("#pera");
+    // p.innerHTML="you have clicked on button 1"
+    var pic1=document.querySelector("#myimg");
+    pic1.src="images/me1.png";
+
+}
+
+function myFunction2(){
+    // var p=document.querySelector("#pera");
+    // p.innerHTML="you have clicked on button 2"
+    var pic1=document.querySelector("#myimg");
+    pic1.src="images/me2.png";
+}
+
+function myFunction3(){
+    alert("hello i am paragraph tag !");
+}
